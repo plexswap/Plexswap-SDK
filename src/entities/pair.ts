7f73a04ec3292e37644adb9cad83cf29769159c5
Symplexia Labs <suporte@symplexia.com.br>
2022-06-01 22:52:52 -0300

    Waya Migration

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -56,7 +56,7 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Cake-LP',
+      'Waya-LP',
       'Plex LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]

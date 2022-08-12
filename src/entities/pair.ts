77774a42191ad0675caf875d4be651c8ef7b1aa8
Symplexia Labs <suporte@symplexia.com.br>
2022-08-12 18:17:48 -0300

    New Name Plex-LP

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -56,7 +56,7 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Waya-LP',
+      'Plex-LP',
       'Plexswap LPs'               //  (**CHECKOUT**)
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
